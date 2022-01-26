741111f4f56dc53b447cc5a253cc1fb5c75e7e3b
hector <hectoreddbenitez@gmail.com>
2022-01-26 15:18:59 -0300

    Correçao requisito 2 e requisito 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,6 +1,6 @@
 -- Conseguí finalizar analizando o PR da Ana Luisa
 SELECT U.nome_usuario AS usuario, 
-	IF(MAX(YEAR(H.data_reproducao)) = "2021", 'Usuário ativo', 'Usuário inativo') AS condiçao_usuario
+	IF(MAX(YEAR(H.data_reproducao)) = "2021", 'Usuário ativo', 'Usuário inativo') AS condicao_usuario
 FROM SpotifyClone.info_usuario U
 INNER JOIN SpotifyClone.historico_de_reproducoes H
 ON U.usuario_id = H.usuario_id
