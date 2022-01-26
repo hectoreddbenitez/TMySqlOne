336e1191664a18cdaa8bfae8ef20eeb35e62514d
hector <hectoreddbenitez@gmail.com>
2022-01-26 14:50:39 -0300

    Requisito 4

---
+++ desafio4.sql
@@ -0,0 +1,8 @@
+-- Conseguí finalizar analizando o PR da Ana Luisa
+SELECT U.nome_usuario AS usuario, 
+	IF(MAX(YEAR(H.data_reproducao)) = "2021", 'Usuário ativo', 'Usuário inativo') AS condiçao_usuario
+FROM SpotifyClone.info_usuario U
+INNER JOIN SpotifyClone.historico_de_reproducoes H
+ON U.usuario_id = H.usuario_id
+GROUP BY nome_usuario
+ORDER BY nome_usuario;
